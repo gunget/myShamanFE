522294211c749da2cf6a3b2c0100cc6511e515f6
travis <aa@aa.com>
2021-03-19 15:08:52 +0900

    ficWriter list 작성중

--- src/components/FicWriterList.jsx
+++ src/components/FicWriterList.jsx
@@ -2,8 +2,31 @@ import React, { useContext } from "react";
 import axios from "axios";
 import { StateContext } from "../contexts/Contexts.jsx";
 
+// material ui import
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+// FicWriter Component
 const FicWriterList = ({ fetchFicWriterInfo }) => {
   const states = useContext(StateContext);
+  const classes = useStyles();
+
+  console.log("ficWritersList comp", states.fictionWriters);
 
   const delPeopleCode = (e) => {
     e.preventDefault();
@@ -23,6 +46,8 @@ const FicWriterList = ({ fetchFicWriterInfo }) => {
   const list = states.loadings.FicWriterInitLoading
     ? "loading..."
     : states.fictionWriters.map((data) => {
+        const link = `https://people.search.naver.com/search.naver?where=nexearch&query=${data.name}&sm=tab_etc&ie=utf8&key=PeopleService&os=${data.peopleCode}`;
+
         return (
           <article
             id={data.name}
@@ -30,9 +55,44 @@ const FicWriterList = ({ fetchFicWriterInfo }) => {
             data-id={data.id}
             data-peoplecode={data.peopleCode}
           >
-            <div>이름:{data.name}</div>
-            <div>PeopleCode:{data.peopleCode}</div>
-            <div>
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  href={link}
+                  className={classes.media}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {data.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button href={link} size="small" color="primary">
+                  Go Naver Search
+                </Button>
+                <Button
+                  data-id={data.id}
+                  onClick={delPeopleCode}
+                  size="small"
+                  color="secondary"
+                >
+                  Delete
+                </Button>
+              </CardActions>
+            </Card>
+            {/* <div>
               <a
                 href="#"
                 data-id={data.id}
@@ -41,7 +101,7 @@ const FicWriterList = ({ fetchFicWriterInfo }) => {
               >
                 <i class="fas fa-trash-alt"></i>&nbsp;&nbsp;delete
               </a>
-            </div>
+            </div> */}
           </article>
         );
       });
