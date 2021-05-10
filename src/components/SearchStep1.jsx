0c9b8637b1710bb6f1822e39cfab141c14b67f1f
travis <aa@aa.com>
2021-05-10 15:15:12 +0900

    searchDirector 영역의 modal창 작업중

--- src/components/SearchStep1.jsx
+++ src/components/SearchStep1.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useContext } from "react";
+import React, { useEffect, useRef, useState, useContext } from "react";
 import axios from "axios";
 import { StateContext } from "../contexts/Contexts.jsx";
 import { DispatchContext } from "../contexts/Contexts.jsx";
@@ -54,6 +54,21 @@ const SearchStep1 = () => {
       });
   };
 
+  //modal control
+  useEffect(() => {
+    const openBtn = document.querySelector("#open");
+    const closeBtn = document.querySelector("#close");
+    const container = document.querySelector(".modal_container");
+
+    openBtn.addEventListener("click", () => {
+      container.classList.add("show");
+    });
+
+    closeBtn.addEventListener("click", () => {
+      container.classList.remove("show");
+    });
+  });
+
   return (
     <div className="container search2">
       <div id="search2" className="alt">
@@ -84,11 +99,46 @@ const SearchStep1 = () => {
           {peopleCode}
         </p>
         <blockquote>
-          - 0번은 검색결과가 없다는 의미입니다. <br /> &nbsp;&nbsp;다른 이름으로
+          - 0은 검색결과가 없다는 의미입니다. <br /> &nbsp;&nbsp;이름을 바꿔
           검색하세요.
-          <br />- 직접번호를 입력하려면 <a href="#">여기</a>를 눌러주세요.
           <br />- 번호가 나오면 다음단계로 진행하세요.
+          <br />- 번호를 직접 입력하려면{" "}
+          <a id="open" style={{ cursor: "pointer" }}>
+            여기
+          </a>
+          를 눌러주세요.
         </blockquote>
+      </div>
+      <div class="modal_container">
+        <div class="modal">
+          <h3>직접 검색하기</h3>
+          <blockquote>
+            1.{" "}
+            <a href="https://movie.naver.com/" target="_blank">
+              네이버 영화
+            </a>
+            &nbsp;-&nbsp;영화검색에서 '작품명'으로 검색, 감독 찾기
+            <br />
+            2. '감독정보'창으로 들어가면 브라우저의 주소줄 클릭하기
+            <br />
+            3. 주소줄 끝부분의 'code='이후의 번호 기억하기
+          </blockquote>
+          <form method="post" action="#" onSubmit={getPeopleCode}>
+            <input
+              type="text"
+              name="query"
+              id="query"
+              placeholder="감독 이름을 입력하세요."
+              ref={inputRef}
+            />
+            <div class="button" onClick={getPeopleCode}>
+              Search
+            </div>
+          </form>
+          <div class="button" id="close">
+            Close
+          </div>
+        </div>
       </div>
     </div>
   );
